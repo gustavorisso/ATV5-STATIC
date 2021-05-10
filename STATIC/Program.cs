a4476b30789ce005f9cd3a168be6ac6a95b3d56f
unknown <gustavorisso2020@gmail.com>
2021-05-10 14:33:06 -0300

    ATIVIDADE INICIADA

---
+++ STATIC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STATIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
